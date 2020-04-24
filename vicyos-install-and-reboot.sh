e5957be5f580b3a6b066d26c1da0e7087c5fc54b
felipendc <felipendc10phoenix@gmail.com>
2020-04-23 23:45:01 -0300

    Update vicyos-install-and-reboot.sh

--- vicyos-install-and-reboot.sh
+++ vicyos-install-and-reboot.sh
@@ -5,7 +5,7 @@
 # Create installation folder:
 sudo mkdir -p /etc/X11/xorg.conf.d/
 
-# Move the file to proper folder:
+# Move the file to the proper folder:
 sudo cp -p 20-intel.conf /etc/X11/xorg.conf.d/
 
 # Reboot to refresh settings:
