3645744e192c63fd893d12afe234bd09a75c15b3
felipendc <felipendc10@gmail.com>
2020-05-07 00:11:33 -0300

    update

--- install-all-scripts-at-once.sh
+++ install-all-scripts-at-once.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 # github.com/felipendc
+# FULL INSTALL COMMING SOON!
 
-#COMMING SOON!
+
+# Synaptic to install deb files.
+sudo apt install synaptic
+
+# Install Firewall and Setup.
+sudo apt install gufw -y
+sudo ufw enable
+sudo ufw allow Samba
+
+# Personal media pkgs
+sudo apt install vlc clementine -y
