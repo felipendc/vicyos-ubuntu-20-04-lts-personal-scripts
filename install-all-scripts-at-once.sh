d40e6611364cb46ab97968b49b5b1227dd8ca894
felipendc <felipendc10@gmail.com>
2020-05-07 00:52:59 -0300

    update

--- install-all-scripts-at-once.sh
+++ install-all-scripts-at-once.sh
@@ -13,7 +13,17 @@ sudo ufw enable
 sudo ufw allow Samba
 
 # Vicyos  Personal packages:
-sudo apt install vlc clementine wine qbittorrent -y
+sudo apt install \
+    vlc clementine wine qbittorrent -y
+
+# Install any pkgs dependencies
+sudo apt install -f
+
+# Auto remove unnecessary pkgs
+sudo apt autoremove
 
 # Vicyos Personal snaps:
-sudo snap install photoscape visualg discord-bartixxx telegram-desktop
+sudo snap install \
+    visualg discord-bartixxx telegram-desktop glimpse-editor \
+    handbrake-jz odio youtube-dl inkscape meteo
+
