3e4a5fd43a8d8047a2cc2a685c959f61164159f4
felipendc <felipendc10@gmail.com>
2020-05-07 01:16:53 -0300

    update

--- install-all-scripts-at-once.sh
+++ install-all-scripts-at-once.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
 # github.com/felipendc
-# FULL INSTALL COMMING SOON!
 
-
-# Synaptic to install deb files:
-sudo apt install synaptic
-
-# Install Firewall and Setup:
-sudo apt install gufw -y
-sudo ufw enable
-sudo ufw allow Samba
 
 # Vicyos  Personal packages:
 sudo apt install \
-    vlc clementine wine qbittorrent -y
+    vlc clementine wine qbittorrent synaptic gufw \
+    samba smbclient samba-common-bin smbclient libsmbclient -y
 
 # Install any pkgs dependencies
 sudo apt install -f
@@ -22,8 +14,32 @@ sudo apt install -f
 # Auto remove unnecessary pkgs
 sudo apt autoremove
 
+# Install Firewall and Setup: 
+sudo ufw enable
+sudo ufw allow Samba
+
+# Setup Samba
+sudo smbpasswd -a vicyos
+
 # Vicyos Personal snaps:
 sudo snap install \
     visualg discord-bartixxx telegram-desktop glimpse-editor \
     handbrake-jz odio youtube-dl inkscape meteo
+
+
+
+
+
+#########################################
+### Run scripts and install packages ####
+#########################################
+
+# Setup Android Build Environment:
+cd android-build-environment-setup && ./android_build_env.sh && cd ../
+
+# Install nitroshare in case samba isn't working:
+cd nitroshare && sudo apt install ./nitroshare_0.3.3-1.1_amd64.deb && cd ../
+
+# Move my personal .bashrc file to my HOME folder:
+cd ubuntu-personal-bashrc && cp -r .bashrc $HOME/ && cd ../
 
--- vicyos-samba-installation-and-setup/install-samba-ubuntu-20-04.sh
+++ vicyos-samba-installation-and-setup/install-samba-ubuntu-20-04.sh
@@ -4,4 +4,4 @@ sudo apt-get install samba
 sudo apt-get install smbclient
 sudo apt-get install samba-common-bin
 sudo apt-get install smbclient libsmbclient
-sudo smbpasswd -a felipendc
+sudo smbpasswd -a vicyos
